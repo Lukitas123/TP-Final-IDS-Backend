7fcd13e9faf579453a513e45b0310b34a5c817af
LauCosen <lautaro_c_19@hotmail.com>
2025-11-26 23:16:20 -0300

    Se pasaron las instrucciones del caso POST a un función aparte que se llama en la función principal.

--- routes/room_types.py
+++ routes/room_types.py
@@ -4,36 +4,39 @@ from psycopg2 import Error as Psycopg2Error
 
 room_types_bp = Blueprint('room_types_bp', __name__)
 
+def get_room_types_post(conn,cur):
+      query = """
+                    INSERT room_type (name, description, gallery)
+                    VALUES (%s, %s, %s)
+                """
+      data_new = request.json
+      name_new = data_new.get("name")
+      description_new = data_new.get("description")
+      gallery_new = data_new.get("gallery")
+      if (not (name_new and description_new and gallery_new)):
+        return jsonify({
+            "status": "error",
+            "message": "No se ingresaron los datos correctamente",
+            }), 409
+        #409 Conflict: El request no se pudo completar debido a un conflicto con el estado actual del recurso
+      else:
+        cur.execute(query,(name_new, description_new, gallery_new))
+        conn.commit()
+        return jsonify({
+            "status": "success",
+            "message": "Datos cargados con éxito",
+            }),200
+
+
 @room_types_bp.route('/room_types', methods=['GET', 'POST'])
 def get_room_types():
     conn = None
     data = None
-    data_new = None
     try:
         conn = get_db_connection()
         with conn.cursor() as cur:
             if (request.method == 'POST'):
-                query = """
-                    INSERT room_type (name, description, gallery)
-                    VALUES (%s, %s, %s)
-                """
-                data_new = request.json
-                name_new = data_new.get("name")
-                description_new = data_new.get("description")
-                gallery_new = data_new.get("gallery")
-                if (not (name_new and description_new and gallery_new)):
-                    return jsonify({
-                        "status": "error",
-                        "message": "No se ingresaron los datos correctamente",
-                    }), 409
-                #409 Conflict: El request no se pudo completar debido a un conflicto con el estado actual del recurso
-                else:
-                    cur.execute(query,(name_new, description_new, gallery_new))
-                    conn.commit()
-                    return jsonify({
-                        "status": "success",
-                        "message": "Datos cargados con éxito",
-                    }),200
+                get_room_types_post(conn,cur)
                 cur.close()
             else:
                 cur.execute('SELECT * FROM room_type ORDER BY id;')
