fea4a6389c636b7d0985dc9c871f6901af815f23
LauCosen <lautaro_c_19@hotmail.com>
2025-11-26 23:34:17 -0300

    Refactor: Se pasaron las instrucciones del caso POST de services a un función aparte que se llama en la función principal.

--- routes/services.py
+++ routes/services.py
@@ -3,37 +3,39 @@ from database import get_db_connection
 
 services_bp = Blueprint('services_bp', __name__)
 
+def get_room_types_post(conn,cur):
+    query = """
+        INSERT INTO service (name, description, price)
+        VALUES (%s, %s, %s)
+    """
+    data_new = request.json
+    name_new = data_new.get("name")
+    description_new = data_new.get("description")
+    price_new = data_new.get("price")
+    if (not (name_new and description_new and price_new)):
+        return jsonify({
+            "status": "error",
+            "message": "No se ingresaron los datos correctamente",
+            }), 409
+    #409 Conflict: El request no se pudo completar debido a un conflicto con el estado actual del recurso
+    else:
+        cur.execute(query,(name_new, description_new, price_new))
+        conn.commit()
+        return jsonify({
+            "status": "success",
+            "message": "Datos cargados con éxito",
+            }),200
+
 @services_bp.route('/services', methods=['GET', 'POST'])
 def get_services():
     conn = None
     data = None
-    data_new = None
     try:
         conn = get_db_connection()
         with conn.cursor() as cur:
 
             if (request.method == 'POST'):
-                query = """
-                    INSERT INTO service (name, description, price)
-                    VALUES (%s, %s, %s)
-                """
-                data_new = request.json
-                name_new = data_new.get("name")
-                description_new = data_new.get("description")
-                price_new = data_new.get("price")
-                if (not (name_new and description_new and price_new)):
-                    return jsonify({
-                        "status": "error",
-                        "message": "No se ingresaron los datos correctamente",
-                    }), 409
-                #409 Conflict: El request no se pudo completar debido a un conflicto con el estado actual del recurso
-                else:
-                    cur.execute(query,(name_new, description_new, price_new))
-                    conn.commit()
-                    return jsonify({
-                        "status": "success",
-                        "message": "Datos cargados con éxito",
-                    })
+                get_room_types_post(conn,cur)
                 cur.close()
             else:
                 #Si es GET
